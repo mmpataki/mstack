316f95abb57e817914d44f77a3fdb60310cbd815
Madhusoodan M Pataki <akshayapataki123@gmail.com>
2017-09-11 00:00:49 +0530

    fixed sorting bugs

--- insert.php
+++ insert.php
@@ -7,17 +7,20 @@
 
 	header("Access-Control-Allow-Origin: *\r\n");
 
-	$conn = mysqli_connect($servername, $username, $password, $dbname);
-	if (!$conn) die("");
+	$conn = new mysqli($servername, $username, $password, $dbname);
+	if ($conn->connect_error) die("");
 
 	if(isset($_GET["from"])) { $start = $_GET["from"]; }
 	$obj = json_decode($_GET["obj"], true);
 
-	$sql = "insert into StackTable values(0, '" . $obj["timestamp"] . "',\"" . $obj["content"] . "\");";
-	if(mysqli_query($conn, $sql)) {
-		$res = mysqli_query($conn, "select max(id) as mid from StackTable");
-		echo mysqli_fetch_assoc($res)["mid"];
-	}
-	mysqli_close($conn);
+	$stmt = $conn->prepare("insert into StackTable values(0,?,?)");
+	$stmt->bind_param("ss", $obj["timestamp"], $obj["content"]);
+
+	$stmt->execute();
+	$res = mysqli_query($conn, "select max(id) as mid from StackTable");
+	echo mysqli_fetch_assoc($res)["mid"];
+
+	$stmt->close();
+	$conn->close();
 ?>
 
